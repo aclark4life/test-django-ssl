36575a95c4f70558ee1ffffd30952a4fe0bcd764
Alex Clark <aclark@aclark.net>
2014-06-24 18:24:36 -0400

    Add file(s)

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    entry_points={
+    },
+    include_package_data=True,
+    install_requires=[
+    ],
+    name='django_ssl_test',
+    namespace_packages=[
+    ],
+    packages=find_packages(),
+)
