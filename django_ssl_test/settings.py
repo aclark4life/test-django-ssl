9aafac63d5852acb0256381e939454918d57bcaa
Alex Clark <aclark@aclark.net>
2014-06-24 18:42:26 -0400

    Alpha sort

--- django_ssl_test/settings.py
+++ django_ssl_test/settings.py
@@ -1,5 +1,3 @@
-# The SECRET_KEY setting must not be empty.
-SECRET_KEY='ZU_hds)oM$%cn$Z8%88x%97Wjx2;8`'
 # settings.DATABASES is improperly configured. Please supply the ENGINE value. Check settings documentation for more details.
 DATABASES = {
     # You must define a 'default' database
@@ -8,3 +6,5 @@ DATABASES = {
         'NAME': 'djangossltest.db',
     }
 }
+# The SECRET_KEY setting must not be empty.
+SECRET_KEY='ZU_hds)oM$%cn$Z8%88x%97Wjx2;8`'
